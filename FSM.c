39e4ea09a76b7dd42b6249d474e61219849588c9
Chris Bove <bove.3123@gmail.com>
2016-02-27 22:59:35 -0500

    development of FSM cases through block speed

--- FSM.c
+++ FSM.c
@@ -17,20 +17,54 @@
  * @brief runs FSM for the final project
  */
 void finiteStateMachine(){
+	static float blockStartTime = 0;
+	static int blockX = 0;
+	static float grabTime = 0;
+
 	static char state = Initialize;
 	switch(state){
 	case Initialize:
 		startConveyor();
 		openGripper();
+		//TODO setXY(CenterX,Waiting_Height+100);
 		state = WaitForBlock;
 		break;
 
 	case WaitForBlock:
-
+		//check if block is sensed (below 110mm)
+		if(IRDist(0) <= Distance_Threshold){
+			blockStartTime = getTimeSeconds(); //save the current time
+			//TODO setXY(CenterX,Waiting_Height);
+			setJointAngles(45,80); //start moving the arm to a nominal position
+			state = CalcBlockX;//move to next state
+		}
 		break;
 
-	case CalcBlockStatus:
+	case CalcBlockX:
+		static int count = 0;
+		static int distanceSum = 0;
 
+		// sample the 1st sensor a few times
+		if(count < 3){
+			distanceSum += IRDist(0); // add values
+			count++;
+		}
+		else{
+			blockX = distanceSum/count;
+			//todo setXY(blockX+CenterX,Waiting_Height);
+			state = CalcBlockSpeed;
+			distanceSum = 0;
+			count = 0;
+		}
+		break;
+	case CalcBlockSpeed:
+		// wait until 2nd sensor is toggled
+		if(IRDist(1)<= Distance_Threshold){
+			float deltaT = getTimeSeconds() - blockStartTime;
+			float velocity = (float) Distance_Between_IR/deltaT; //mm/sec
+			grabTime = Distance_IR_To_Arm/velocity; // time until block goes in front of arm
+			state = GenerateTrajectoryGrab;
+		}
 		break;
 	case GenerateTrajectoryGrab:
 
--- include/FSM.h
+++ include/FSM.h
@@ -18,7 +18,8 @@
 enum FSMStates {
 	Initialize,
 	WaitForBlock,
-	CalcBlockStatus,
+	CalcBlockX,
+	CalcBlockSpeed,
 	GenerateTrajectoryGrab,
 	ExecuteGrabMotion,
 	GrabBlock,
@@ -28,6 +29,36 @@ enum FSMStates {
 	ExecuteDropMotion,
 	DropBlock
 };
+
+/**
+ * @def Waiting_Height
+ * height in mm arm will wait above conveyor belt once block is detected
+ * @def Center_X
+ * x coordinate arm will wait at once block is detected
+ */
+#define Waiting_Height 110
+#define Center_X 125
+
+/**
+ * @def Distance_Threshold
+ * distance in mm to trip the IR sensor
+ * @def Distance_Between_IR
+ * distance in mm between IR sensors
+ * @def Distance_IR_To_Arm
+ * distance in mm between last IR sensor and Arm
+ */
+#define Distance_Threshold 110
+#define Distance_Between_IR 100
+#define Distance_IR_To_Arm 250
+
+/**
+ * @def Time_To_Move
+ * The time before the block grab time to start moving down
+ * @def Time_To_Grab
+ * The time before the block grab time to request a gripper close
+ */
+#define Time_To_Move 1.0
+#define Time_To_Grab 0.4
 
 /**
  * @brief runs FSM for the final project
